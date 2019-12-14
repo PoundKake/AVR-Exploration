6b8649856cffe5b27f9a8d6767fca4217d5ef005
PoundKake <vance.christian@gmail.com>
2019-12-14 16:44:39 -0500

    Refactored the ATmega328p/Blink project to be more modulare through the use of funtions. Started an empty project called ATtiny85/Blink to explore the functionality and limits of the ATtiny85. Last Working Comit.

--- ATmega328p/Blink/Blink/main.c
+++ ATmega328p/Blink/Blink/main.c
@@ -4,28 +4,49 @@
  * Created: 12/7/2019 15:45:20
  * Author : Christian
  */ 
+// Macro Definitions
 #define F_CPU 16000000UL
 
+// Includes
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
 
+// Global variable definitions.
+const float DELAY_VAL_MS = 1000.00;	// This is how many milliseconds the delay function will delay.
+
+/**
+ * Takes the designated pin number and toggles it on and off dictated by the defined parameter for delay.
+ *
+ * @param pinNum char
+ * @param delay float
+ * @return void
+ */
+void blinkOnBoardLED(char pinNum, float delay) 
+{ 
+	delay = DELAY_VAL_MS ;	// This immediately assigns the value of delay to the globally defined value to avoid compilation errors.
+	PORTB = pinNum; 
+	_delay_ms(delay);
+	PORTB = 0x00;
+	_delay_ms(delay);
+}
+
 // Main function
 int main(void)
 {
-	
-	const float delay_val_ms = 1000.00;	// This is how many milliseconds the delay function will delay.
-	unsigned char G = 127;				// This is the amplitude of the sine function
-	DDRB |= 0x06;						// This defines the DDRx Register for PORTB. This should be 0b0000110
+	DDRB |= 0x06;						// This defines the DDRx Register for PORTB. This should be 0000 0110
+	//DDRB |= 0x20;						// This defines the DDRx Register for PORTB. This should be 0001 0000
+	char pin5 = 0x20;
 	
     /* Replace with your application code */
     while (1) 
     {
 		// This blinks an led on and off. Useful for debugging and testing of the MCU control
-		PORTB |= (1 << 3);
-		_delay_ms(delay_val_ms);
-		PORTB |= (1 << 2);
-		_delay_ms(delay_val_ms);
+		blinkOnBoardLED(pin5, DELAY_VAL_MS);
 	}
+	return 0;
 }
+
+//PORTB |= (1 << 3);
+//PORTB |= (1 << 2);
---
+++ ATtiny85-20p/tiny-Blink/tiny-Blink/main.c
@@ -0,0 +1,18 @@
+/*
+ * tiny-Blink.c
+ *
+ * Created: 12/14/2019 15:04:13
+ * Author : Christian
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
